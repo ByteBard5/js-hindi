725413bfd51df29e20c41ee6d330a62e5fd297a9
Yash Maheshwari <147691638+ByteBard5@users.noreply.github.com>
2024-04-14 19:16:19 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) //we are using node JS, not browser
+
+// console.log(3 + 3); console.log("yash") // it will word but that's not a good way
+
+let name = "Yash"
+let age = 22
+let isLoggedIn = true
+
+// number => 2 to power 53
+// BigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+console.log(typeof "yash");
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
