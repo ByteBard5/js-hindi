cf6493fe61d651aefc548aa72b1370ee5d7dc2d2
Yash Maheshwari <147691638+ByteBard5@users.noreply.github.com>
2024-04-18 08:40:31 +0000

    operators

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -21,15 +21,47 @@
 //  "33abc" => NaN
 //  true => 1; false => 0
 
-let isLoggedIn = 1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 //  1 => true; 0 => false
 //  "" => false
 //  "yash" => true
 
-let someNumber = 33
-let stringNumber = String(sumNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber)
+// let someNumber = 33
+// let stringNumber = String(sumNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+
+// ****************************operations**********************************
+
+// let value = 3
+// let negValue = -3
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "Hello"
+// let str2 = " Yash"
+
+// console.log(true);
+// console.log(+true);
+// console.log(true+);
+
+// console.log(+"");
+
+// let gameCounter = 100
+// gameCounter++ ;
+// console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
